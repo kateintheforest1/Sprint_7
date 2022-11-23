60d2c8f2e55782fdff924ab1b9fb33fcbec0e546
Ekaterina Kuznetsova <108665826+kateintheforest1@users.noreply.github.com>
2022-11-23 22:53:04 +0500

    courier login refactoring

--- src/test/java/CourierApi.java
+++ src/test/java/CourierApi.java
@@ -34,4 +34,13 @@ public class CourierApi {
             given().delete("/api/v1/courier/" + courierId);
         }
     }
+
+    public Response login(File json) {
+        return given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(json)
+                .when()
+                .post("/api/v1/courier/login");
+    }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,5 +1,4 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Before;
@@ -10,8 +9,12 @@ import java.io.File;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-
 public class LoginTest {
+    private CourierApi courierApi;
+    public LoginTest() {
+        CourierApi api = new CourierApi();
+        this.courierApi = api;
+    }
 
     @Before
     public void setUp() {
@@ -20,47 +23,38 @@ public class LoginTest {
 
     public void createNewCourier() {
         File json = new File("src/test/resources/newCourier.json");
+        courierApi.create(json);
         Response response = given().header("Content-type", "application/json").and().body(json).when().post("/api/v1/courier");
     }
 
     @After
     public void cleanUp() {
-        deleteCourier(new File("src/test/resources/newCourier.json"));
+        courierApi.delete(new File("src/test/resources/newCourier.json"));
     }
 
 
-        @Test
-        @DisplayName("Check login by courier")
-        public void loginCourierTest() {
-            createNewCourier();
-            File json = new File("src/test/resources/loginCourier.json");
-            Response response1 =
-                    given()
-                            .header("Content-type", "application/json")
-                            .and()
-                            .body(json)
-                            .when()
-                            .post("/api/v1/courier/login");
-            response1.then().assertThat().statusCode(200).and().body("id", notNullValue());
+    @Test
+    @DisplayName("Check login by courier")
+    public void loginCourierTest() {
+        createNewCourier();
 
-            //deleteCourier(json);
-        }
+        File json = new File("src/test/resources/loginCourier.json");
+        courierApi
+                .login(json)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("id", notNullValue());
+    }
 
     @Test
     @DisplayName("Check login by courier without entering login")
     public void loginCourierWithoutLoginTest() {
         createNewCourier();
-        File json = new File("src/test/resources/withoutLoginCourier.json");
-        Response response1 =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response1.then().assertThat().statusCode(400).and().body("message", equalTo("Недостаточно данных для входа"));
 
-        //deleteCourier(json);
+        File json = new File("src/test/resources/withoutLoginCourier.json");
+        courierApi.login(json).then().assertThat().statusCode(400).and().body("message", equalTo("Недостаточно данных для входа"));
     }
 
     @Test
@@ -68,70 +62,24 @@ public class LoginTest {
     public void loginCourierWithoutPasswordTest() {
         createNewCourier();
         File json = new File("src/test/resources/onlyLoginCourier.json");
-        Response response1 =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response1.then().assertThat().statusCode(400).and().body("message", equalTo("Недостаточно данных для входа"));
-
-        //deleteCourier(json);
+        courierApi.login(json).then().assertThat().statusCode(400).and().body("message", equalTo("Недостаточно данных для входа"));
     }
 
     @Test
     @DisplayName("Check login by courier with incorrect login")
     public void loginCourierNotCorrectTest() {
         createNewCourier();
+
         File json = new File("src/test/resources/loginNotCorrectCourier.json");
-        Response response1 =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response1.then().assertThat().statusCode(404).and().body("message", equalTo("Учетная запись не найдена"));
-
-        //deleteCourier(json);
+        courierApi.login(json).then().assertThat().statusCode(404).and().body("message", equalTo("Учетная запись не найдена"));
     }
-
 
     @Test
     @DisplayName("Check login by courier with incorrect password")
     public void loginCourierNotCorrectPasswordTest() {
         createNewCourier();
         File json = new File("src/test/resources/loginNotCorrectPassword.json");
-        Response response1 =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-        response1.then().assertThat().statusCode(404).and().body("message", equalTo("Учетная запись не найдена"));
-
-        //deleteCourier(json);
+        courierApi.login(json).then().assertThat().statusCode(404).and().body("message", equalTo("Учетная запись не найдена"));
     }
-
-    public void deleteCourier(File courierJson) {
-        Response response0 =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(courierJson)
-                        .when()
-                        .post("/api/v1/courier/login");
-
-        JsonPath jsonBody = response0.body().jsonPath();
-
-        if(response0.statusCode() == 200) {
-            String courierId = jsonBody.getString("id");
-            //Response deleteResponse =
-            given()
-                    .delete("/api/v1/courier/" + courierId);
-        }
-    }
-    }
+}
 
