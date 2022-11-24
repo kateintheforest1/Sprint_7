f95c8f08eec1ae5a089ea5b58a600ee455c4b87e
Ekaterina Kuznetsova <108665826+kateintheforest1@users.noreply.github.com>
2022-11-24 22:52:24 +0500

    LoginTest refactoring

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,6 @@
 import com.google.gson.Gson;
 import io.qameta.allure.Step;
-import io.restassured.RestAssured;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import io.qameta.allure.junit4.DisplayName;
 
 import static org.apache.http.HttpStatus.*;
@@ -39,8 +36,6 @@ public class LoginTest {
     @DisplayName("Check login by courier")
     @Step("Send POST request to /api/v1/courier/login and check response status")
     public void loginCourierTest() {
-        createNewCourier();
-
         Courier courier = new Courier();
         courier.setLogin("ldld");
         courier.setPassword("1234");
@@ -58,9 +53,7 @@ public class LoginTest {
     @DisplayName("Check login by courier without entering login")
     @Step("Send POST request to /api/v1/courier/login without entering login and check response status")
     public void loginCourierWithoutLoginTest() {
-        createNewCourier();
         Courier courier = new Courier();
-
         courier.setPassword("1234");
 
         courierApi.login(courier).then().assertThat().statusCode(SC_BAD_REQUEST).and().body("message", equalTo("Недостаточно данных для входа"));
@@ -70,10 +63,10 @@ public class LoginTest {
     @DisplayName("Check login by courier without entering password")
     @Step("Send POST request to /api/v1/courier/login without entering password and check response status")
     public void loginCourierWithoutPasswordTest() {
-        createNewCourier();
         Courier courier = new Courier();
         courier.setLogin("ldld");
         courier.setPassword("");
+
         courierApi.login(courier).then().assertThat().statusCode(SC_BAD_REQUEST).and().body("message", equalTo("Недостаточно данных для входа"));
     }
 
@@ -81,8 +74,6 @@ public class LoginTest {
     @DisplayName("Check login by courier with incorrect login")
     @Step("Send POST request to /api/v1/courier/login with entering incorrect login and check response status")
     public void loginCourierNotCorrectTest() {
-        createNewCourier();
-
         Courier courier = new Courier();
         courier.setLogin("kjlkj");
         courier.setPassword("1234");
@@ -93,7 +84,6 @@ public class LoginTest {
     @DisplayName("Check login by courier with incorrect password")
     @Step("Send POST request to /api/v1/courier/login with entering incorrect password and check response status")
     public void loginCourierNotCorrectPasswordTest() {
-        createNewCourier();
         Courier courier = new Courier();
         courier.setLogin("ldld");
         courier.setPassword("ljljh");
